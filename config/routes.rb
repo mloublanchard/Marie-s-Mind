1cef9e70145c0fb448d9e9dd1f83b4818da4f440
Marie-Lou Blanchard <mloublanchard@gmail.com>
2015-06-30 13:08:23 -0400

    Load pins in index action in reverser chronological order

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
  root "pins#index"
- get "mission" => "pages#mission"
+ get "purpose" => "pages#purpose"
  get "contact" => "pages#contact"
  get "product" => "pages#product"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
